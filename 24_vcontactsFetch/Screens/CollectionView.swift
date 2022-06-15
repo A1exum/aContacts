5a7a25ce39ea9333b6d9313ea0fd01b19f0d058a
alex <f2100@yandex.ru>
2022-06-15 19:21:22 +0300

    build(dev): add resources files

---
+++ 24_vcontactsFetch/Screens/CollectionView.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionView.swift
+//  22_vcontacts
+//
+//  Created by admin on 24.05.22.
+//
+
+import UIKit
+
+class CollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
